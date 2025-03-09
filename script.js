2e4702384f3c157b86c5db7403d2c47131594114
vianawill <willian.v.f.wv@gmail.com>
2025-03-09 13:28:47 -0300

    fix = correção de ortográfia

--- script.js
+++ script.js
@@ -272,7 +272,7 @@ checkoutBtn.addEventListener("click", function(){
 function checkRestaurantOpen(){
     const data = new Date();
     const hora = data.getHours();
-    return hora >= 13 && hora < 22; // true 
+    return hora >= 18 && hora < 22; // true 
 }
 
 // Verificação que muda a cor do span
